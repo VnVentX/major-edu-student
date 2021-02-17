947de486215717af923953d8846687d0bbb733bc
Uyen <uyen.tu.277@gmail.com>
2021-02-17 22:55:53 +0700

    Notice Components
    Notice Components

--- src/components/subject/SubjectComponent.js
+++ src/components/subject/SubjectComponent.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Image } from 'antd';
+
 
 const SubjectComponent = () => {
   return (
     <div className="page">
       <div className="page-contain">
+      <Image
+      width={200}
+      src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+      />
         <Link to="math/unit">
           <button>Math</button>
         </Link>
+        <Image
+      width={200}
+      src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+      />
         <Link to="science/unit">
           <button>Science</button>
         </Link>
