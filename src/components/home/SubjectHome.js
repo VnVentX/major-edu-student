8d3ae9962be43f7538bc9efdb30283213987f41f
Uyen <uyen.tu.277@gmail.com>
2021-02-24 14:21:46 +0700

    Subject Component (uyen)
    Subject Component

--- src/components/home/SubjectHome.js
+++ src/components/home/SubjectHome.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Image } from 'antd';
 
 const SubjectHome = () => {
   return (
@@ -6,7 +8,33 @@ const SubjectHome = () => {
     //   <div className="page-contain">
     <div className="subject-home">
       <div className="subject-home-container">
-        <div className="subjects-home-content"></div>
+        <div className="subjects-home-content">
+        <div className="subject-page-container">
+        <div className="lets-started"/>
+        <div className="subject-page-sub">
+          <div className="subject-science">
+            <Image
+              width={200}
+              preview={false}
+              src="https://www.schandpublishing.com/BookImages/schand-size180/9789352834389.jpg"
+            />
+            <Link to="science/unit">
+              <button className="button-subject-science">Science</button>
+            </Link>
+          </div>
+          <div className="subject-math ">
+            <Image
+              preview={false}
+              width={200}
+              src="https://www.wamc.org/sites/wamc/files/styles/x_large/public/201811/math-with-bad-drawings.jpg"
+            />
+            <Link to="math/unit">
+              <button className="button-subject-math">Math</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+        </div>
       </div>
     </div>
     //   </div>
