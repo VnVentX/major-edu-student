86db7814c39a6a8d7f7199b46587c316688ffc7b
VnVentX <51308208+VnVentX@users.noreply.github.com>
2021-02-24 15:45:58 +0700

    Update Home & Subject Design

--- src/components/home/SubjectHome.js
+++ src/components/home/SubjectHome.js
@@ -1,44 +1,40 @@
 import React from "react";
+import math from "../../resources/img/home/math.png";
 import { Link } from "react-router-dom";
-import { Image } from 'antd';
 
 const SubjectHome = () => {
   return (
-    // <div className="page">
-    //   <div className="page-contain">
     <div className="subject-home">
-      <div className="subject-home-container">
-        <div className="subjects-home-content">
-        <div className="subject-page-container">
-        <div className="lets-started"/>
-        <div className="subject-page-sub">
-          <div className="subject-science">
-            <Image
-              width={200}
-              preview={false}
-              src="https://www.schandpublishing.com/BookImages/schand-size180/9789352834389.jpg"
-            />
-            <Link to="science/unit">
-              <button className="button-subject-science">Science</button>
-            </Link>
+      <div className="page">
+        <div className="page-contain">
+          <div className="subject-home-container">
+            <div className="subject-home-title" />
+            <div className="subject-home-content">
+              <div className="subject-home-wrap">
+                <div className="subject-outter-border">
+                  <div className="subject-inner-border">
+                    <img src={math} alt={math} />
+                  </div>
+                </div>
+                <Link to="/math/unit">
+                  <div className="math-btn" />
+                </Link>
+              </div>
+              <div className="subject-home-wrap">
+                <div className="subject-outter-border">
+                  <div className="subject-inner-border">
+                    <img src={math} alt={math} />
+                  </div>
+                </div>
+                <Link to="/science/unit">
+                  <div className="science-btn" />
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="subject-math ">
-            <Image
-              preview={false}
-              width={200}
-              src="https://www.wamc.org/sites/wamc/files/styles/x_large/public/201811/math-with-bad-drawings.jpg"
-            />
-            <Link to="math/unit">
-              <button className="button-subject-math">Math</button>
-            </Link>
-          </div>
-        </div>
-      </div>
         </div>
       </div>
     </div>
-    //   </div>
-    // </div>
   );
 };
 
