b7c8ffa14e112fd4d7564ec6319c2687d7d14f0d
phuongttt <beatrixfptedu@gmail.com>
2021-04-06 23:08:39 +0700

    profile page
    chưa background

--- src/components/profile/ProfileComponent.js
+++ src/components/profile/ProfileComponent.js
@@ -1,11 +1,25 @@
 import React from "react";
+import bg from "../../resources/img/profile/profile-bg.png";
 
 const ProfileComponent = () => {
   return (
     <div className="page">
       <div className="page-contain">
-          
+      <div className="profile-detail-container">
+        <div className="profile-frame">
+
+          <div className="avatar-frame">
+
+          </div>
+          <div className="name-place">
+            <h1> Name: PhuongTTT</h1>
+            <h1> Class: 5 A1</h1>
+            <h1>School: Major</h1>
+          </div>
+
+        </div>
       </div>
+    </div>
     </div>
   );
 };
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../resources/css/profile.css";
 import ProfileComponent from "../components/profile/ProfileComponent";
 
 const Profile = () => {
